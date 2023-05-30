42525b6c5601ad7d1409567da09159ecc964a31f
Jaime Mantas de Torres <jmantas@ayesa.es>
2023-05-30 09:46:19 +0200

    Inicialización

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RedesSocialesComponent } from './redes-sociales/redes-sociales.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearcherComponent } from './searcher/searcher.component';
+import { EnlacesComponent } from './enlaces/enlaces.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RedesSocialesComponent,
+    NavbarComponent,
+    SearcherComponent,
+    EnlacesComponent
   ],
   imports: [
     BrowserModule,
